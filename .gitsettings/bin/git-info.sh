19e1a07599a02680cd351053ffcbbbe5451ac3d6
Gavin Corbett <gav.corbett@gmail.com>
2013-04-15 10:51:23 +0100

    Update to make git shell scripts executable

--- .gitsettings/bin/git-info.sh
+++ .gitsettings/bin/git-info.sh
@@ -14,27 +14,29 @@ while [ ! -d .git ] && [ ! `pwd` = "/" ]; do cd ..; done
 
 # Show various information about this git directory
 if [ -d .git ]; then
-echo "== Remote URL: `git remote -v`"
+    echo "== Remote URL: "
+    git remote -v
+    echo
 
-echo "== Remote Branches: "
-git branch -r
-echo
+    echo "== Remote Branches: "
+    git branch -r
+    echo
 
-echo "== Local Branches:"
-git branch
-echo
+    echo "== Local Branches:"
+    git branch
+    echo
 
-echo "== Configuration (.git/config)"
-cat .git/config
-echo
+    echo "== Configuration (.git/config)"
+    cat .git/config
+    echo
 
-echo "== Most Recent Commit"
-git —no-pager log —max-count=1
-echo
+    echo "== Most Recent Commit"
+    git —no-pager log —max-count=1
+    echo
 
-echo "Type 'git log' for more commits, or 'git show' for full commit details."
-else
-echo "Not a git repository."
+    echo "Type 'git log' for more commits, or 'git show' for full commit details."
+    else
+    echo "Not a git repository."
 fi
 
 popd >/dev/null
